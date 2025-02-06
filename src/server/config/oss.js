d429fc6c17d7bf7e1d58f5572a7571a9c534901a
JustaNewer <your.email@example.com>
2025-02-06 23:54:33 +0800

    refactor: move OSS credentials to environment variables

---
+++ src/server/config/oss.js
@@ -0,0 +1,6 @@
+module.exports = {
+    region: 'oss-cn-beijing',
+    accessKeyId: process.env.OSS_ACCESS_KEY_ID,
+    accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
+    bucket: 'mateallocation'
+};
